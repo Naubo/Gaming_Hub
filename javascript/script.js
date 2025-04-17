2b04afab474614bb22d5432ae8c7a4a2f741096d
Nauber Souza <149209805+Naubo@users.noreply.github.com>
2025-04-17 20:33:27 -0300

    CORREÇÃO JS

--- javascript/script.js
+++ javascript/script.js
@@ -15,7 +15,11 @@ $(document).ready(function () {
     const header = $("#header");
     const scrollPosition = $(Window) - header.outerHeight();
 
-
+    if (scrollPosition <=0) {
+        header.css('box-shadow', 'none');
+    } else {
+        header.css('box-shadow'), '5px 1px 5px rgba(0, 0, 0, 0.1)';
+    }
     
-  });
-});
+  })
+})
