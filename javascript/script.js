dfbe5cb7190decf9749eba21f7ed618e9aad4041
Nauber Souza <149209805+Naubo@users.noreply.github.com>
2025-04-17 20:19:20 -0300

    correções e descrição

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,21 @@
 /* Iniciando o Jquery*/
-$(document).ready(function() {
-    $('#mobile_btn').on('click', function (){
-        $('#mobile_menu').toggleClass('active');
+$(document).ready(function () {
+  $("#mobile_btn").on("click", function () {
+    $("#mobile_menu").toggleClass("active");
 
-        // trocar icone para x
-        $('#mobile_btn').find('i').toggleClass('fa-x');
+    // trocar icone para x
+    $("#mobile_btn").find("i").toggleClass("fa-x");
+  });
+  // selecionar as sessões e os icones de cada
+  const sections = $("section");
+  const navitens = $(".nav-item");
 
-    })
-})
+  // função de quando scrollar a página
+  $(Window).on("scroll", function () {
+    const header = $("#header");
+    const scrollPosition = $(Window) - header.outerHeight();
+
+
+    
+  });
+});
