76875ace87cab5996183111510857bf420c79c4c
Nauber Souza <149209805+Naubo@users.noreply.github.com>
2025-04-18 17:25:34 -0300

    JavaScript Correções

--- javascript/script.js
+++ javascript/script.js
@@ -10,16 +10,32 @@ $(document).ready(function () {
   const sections = $("section");
   const navitens = $(".nav-item");
 
-  // função de quando scrollar a página
+  // função de quando scrollar a página fazer sombra no header
   $(Window).on("scroll", function () {
-    const header = $("#header");
-    const scrollPosition = $(Window) - header.outerHeight();
+    const header = $("header");
+    const scrollPosition = $(Window).scrollTop() - header.outerHeight();
 
-    if (scrollPosition <=0) {
-        header.css('box-shadow', 'none');
+    let activeSectionIndex = 0;
+
+    if (scrollPosition <= 0) {
+      header.css("box-shadow", "none");
     } else {
-        header.css('box-shadow'), '5px 1px 5px rgba(0, 0, 0, 0.1)';
+      header.css("box-shadow", "5px 1px 5px rgba(0, 0, 0, 0.1");
     }
-    
-  })
-})
+
+    sections.each(function (i) {
+      const section = $(this);
+      const sectionTop = section.offset().top - 96;
+      const sectionBottom = sectionTop + section.outerHeight();
+
+      if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+        activeSectionIndex = i;
+        return false; // sair do loop se a seção ativa for encontrada
+      }
+    });
+
+    // se não for encontrada, o index vai ser
+    navitens.removeClass("active");
+    $(navitens[activeSectionIndex]).addClass("active");
+  });
+});
